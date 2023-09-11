1ebd448a6cfe07e4addcc2285288415f49759c20
Brandon Belna <bbelna@inheret.com>
2023-09-11 12:56:31 -0400

    Initial code for my website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from 'src/app/components/landing/landing.component';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const COMPONENTS = [
+  AppComponent,
+  LandingComponent,
+  HeaderComponent
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [...COMPONENTS],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
